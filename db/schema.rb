3db8d71c3c5713765d660c8a118a52caf4f0dbb7
Daniel Ronan <danielconnorronan@gmail.com>
2020-10-29 14:43:18 +0000

    generate Event model and add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_141419) do
+ActiveRecord::Schema.define(version: 2020_10_29_143755) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
