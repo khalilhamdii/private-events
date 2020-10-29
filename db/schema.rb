04f871268caebb7a6bfb1b61a8d0c1c5173f8c1b
khalilhamdii <khaalil.hamdi@gmail.com>
2020-10-29 14:02:59 +0100

    Add user controller

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_124229) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
