5ba5bd25d51bba018ccd9da934cee69ffdad60f5
Daniel Ronan <danielconnorronan@gmail.com>
2020-11-02 13:05:05 +0000

    add attendee associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+
+  has_many :event_attendences, foreign_key: :attended_event_id, class_name: "EventAttendence"
+  has_many :attendees, through: :event_attendences
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
+
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+
+  has_many :event_attendences, foreign_key: :attendee_id, class_name: "EventAttendence"
+  has_many :attended_events, through: :event_attendences
 end
