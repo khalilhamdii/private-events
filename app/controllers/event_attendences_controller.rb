aec491c9c1da65fd48866e1af97f4d91f55c398d
Daniel Ronan <danielconnorronan@gmail.com>
2020-11-02 15:34:15 +0000

    add solution to event attendances and attend btn

--- app/controllers/event_attendences_controller.rb
+++ app/controllers/event_attendences_controller.rb
@@ -1,12 +1,24 @@
 class EventAttendencesController < ApplicationController
-  def new
-    @event_attendence = EventAttendence.new
+  def create
+    @event_attendence = EventAttendence.new(attended_event_id: params[:id], attendee_id: current_user.id)
+    @event_attendence.save
+    redirect_to signup_path
   end
 
-  def create
-    @event_attendence = EventAttendence.new
-    @event_attendence.attended_event_id = params["event_id"]
-    @event_attendence.attendee_id = current_user.id
-    @event_attendence.save
-  end
+  # def create
+  #   @event_attendence = EventAttendence.new
+  #   @event_attendence.attended_event_id = params["event_id"]
+  #   @event_attendence.attendee_id = current_user.id
+  #   @event_attendence.save
+  # end
+
+  # def create
+  #   @event_attendence = EventAttendence.new(event_attendence_params)
+  # end
+
+  # private
+
+  # def event_attendence_params
+  #   params.require(:event_attendence).permit(:event_id, :user_id)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'home', to: 'events#index', as: 'home'
-
 end
