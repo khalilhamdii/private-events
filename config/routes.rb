f49d80a2901cc336fae0112c17caf1e50c7f31b5
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-02 13:42:09 +0100

    Add users and events routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'events#new'
-  resources :users, only: [:new, :create]
-  resources :events, only: [:show, :create]
+  root 'events#index'
+  resources :users
+  resources :events
+  resources :event_attendences, only: [:new, :create]
+  resources :sessions, only: [:index, :new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'home', to: 'events#index', as: 'home'
+
 end
