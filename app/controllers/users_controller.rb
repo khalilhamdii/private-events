6aa81bb743785591c282fbaa83241475bf43b9a2
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-02 13:39:37 +0100

    Edit user controller + add current_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,62 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
+  def show
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to edit_user_path
-    else
-      render :edit
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
     end
   end
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
