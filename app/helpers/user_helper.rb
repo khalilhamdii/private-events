804a0122b19323e77e93b4c34087a1ea65525912
khalilhamdii <khaalil.hamdi@gmail.com>
2020-11-03 13:22:35 +0100

    Adjust  user helpers methods

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,25 +1,26 @@
 module UserHelper
+
   def future_events(event, date)
+    tmp = content_tag :div, :class => "field d-flex justify-content-between" do
+      concat(content_tag(:h4, event.title, class: "text-secondary") + " " +  (content_tag(:div, event.date, class: "text-success")))
+    end
+    container = content_tag :div, :class => "bg-light p-2 mb-2" do
+      tmp + content_tag(:div, event.description)
+    end
     if date > Time.now
-      content_tag :div, :class => "bg-light p-4 mb-2 d-flex justify-content-between" do
-        concat(
-          content_tag(:h4, event.title, class: "text-info") +
-          content_tag(:div, event.description) +
-          content_tag(:div, event.date, class: "text-warning")
-        )
-      end
+      container
     end
   end
 
-  def past_events(event, date)
-    if date < Time.now
-      content_tag :div, :class => "bg-light p-4 mb-2 d-flex justify-content-between" do
-        concat(
-          content_tag(:h4, event.title, class: "text-info") +
-          content_tag(:div, event.description) +
-          content_tag(:div, event.date, class: "text-warning")
-        )
-      end
+  def previous_events(event, date)
+    tmp = content_tag :div, :class => "field d-flex justify-content-between" do
+      concat(content_tag(:h4, event.title, class: "text-secondary") + " " +  (content_tag(:div, event.date, class: "text-success")))
+    end
+    container = content_tag :div, :class => "bg-light p-2 mb-2" do
+      tmp + content_tag(:div, event.description)
+    end
+    if Time.now > date
+      container
     end
   end
 end
