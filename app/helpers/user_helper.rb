75018cb2e6ea75214753765d4ec2f6b9693daacb
Daniel Ronan <danielconnorronan@gmail.com>
2020-11-03 10:46:51 +0000

    add two helpers for show events and edit events show erb

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,25 @@
 module UserHelper
+  def future_events(event, date)
+    if date > Time.now
+      content_tag :div, :class => "bg-light p-4 mb-2 d-flex justify-content-between" do
+        concat(
+          content_tag(:h4, event.title, class: "text-info") +
+          content_tag(:div, event.description) +
+          content_tag(:div, event.date, class: "text-warning")
+        )
+      end
+    end
+  end
+
+  def past_events(event, date)
+    if date < Time.now
+      content_tag :div, :class => "bg-light p-4 mb-2 d-flex justify-content-between" do
+        concat(
+          content_tag(:h4, event.title, class: "text-info") +
+          content_tag(:div, event.description) +
+          content_tag(:div, event.date, class: "text-warning")
+        )
+      end
+    end
+  end
 end
